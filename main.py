5b75252c11cc6c4d2b08e7e231237f281d2a018b
Mantas M. <mantaskzkp@gmail.com>
2025-07-05 18:59:10 +0100

    First commit for asteroid game

---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame
+from CONSTANTS import *
+from player import Player
+
+def main():
+    pygame.init()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        updatable.update(dt)
+        screen.fill("black")
+
+        for draw in drawable:
+            draw.draw(screen)
+
+        pygame.display.flip()
+
+        dt = clock.tick(60)/1000
+
+if __name__ == "__main__":
+    main()
